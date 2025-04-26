de5fb9a73693080d670f2e1133cf996b53cca685
Big Sam <stosin525@gmail.com>
2025-04-26 21:34:56 +0100

    Implemented a logo slider under the hero section using swiperjs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "./HeroSection";
+import LogoSlider from "./LogoSlider";
 import ProductCategories from "./ProductCategories";
 import TrendingProducts from "./TrendingProducts";
 import GeneralReviews from "./GeneralReviews";
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <main className="w-full h-full mb-10">
       <HeroSection />
+      <LogoSlider />
       <ProductCategories />
       <TrendingProducts />
       <GeneralReviews />
