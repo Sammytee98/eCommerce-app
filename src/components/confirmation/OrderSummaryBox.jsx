536676715b14403bcd0d673bd7bb520d4339d195
Big Sam <stosin525@gmail.com>
2025-05-21 22:45:14 +0100

    modifed the order summary in confirmation page

--- src/components/confirmation/OrderSummaryBox.jsx
+++ src/components/confirmation/OrderSummaryBox.jsx
@@ -17,29 +17,35 @@ const OrderSummaryBox = () => {
     <section className="min-w-xs p-4 grid grid-cols-1 tablet:space-x-6 space-y-4 tablet:grid-cols-2 bg-gray-100 rounded-md shadow-md">
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Order Number</p>
-        <p className="text-base text-gray-800 font-medium">{orderNumber}</p>
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
+          {orderNumber}
+        </p>
       </div>
 
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Date Placed</p>
-        <p className="text-base text-gray-800 font-medium">
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
           {formattedOrderDate}
         </p>
       </div>
 
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Email</p>
-        <p className="text-base text-gray-800 font-medium">{shipEmail}</p>
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
+          {shipEmail}
+        </p>
       </div>
 
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Total Paid</p>
-        <p className="text-base text-gray-800 font-medium">${totalPaid}</p>
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
+          ${totalPaid}
+        </p>
       </div>
 
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Payment Method</p>
-        <p className="text-base text-gray-800 font-medium">
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
           {paymentMethod.paypal
             ? "Paypal"
             : paymentMethod.cod
@@ -50,7 +56,7 @@ const OrderSummaryBox = () => {
 
       <div className="flex flex-col space-y-1 text-sm text-gray-500">
         <p>Estimated Delivery</p>
-        <p className="text-base text-gray-800 font-medium">
+        <p className="text-base tablet:text-lg text-gray-800 font-medium">
           {`${formattedOrderDate} - ${formattedEstimedDate}`}
         </p>
       </div>
